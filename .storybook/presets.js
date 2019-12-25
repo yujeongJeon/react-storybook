fe1e90abfe7565e48796fcc8e6181d4b6d1b98bf
ldcc-juj <dbwjd3508@lotte.net>
2019-12-25 23:55:29 +0900

    first commit by newjeong

---
+++ .storybook/presets.js
@@ -0,0 +1,4 @@
+module.exports = ['@storybook/addon-docs/react/preset'];
+/**
+ * Storybook presets are grouped collections of babel, webpack, and addons configurations that support specific use cases.
+ */
